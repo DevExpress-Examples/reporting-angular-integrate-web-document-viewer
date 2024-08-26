4ac79726d1d670ab2434a08f990c2516540c6484
Yaroslav Kuznetsov <yaroslav.kuznetsov@devexpress.com>
2024-08-27 00:35:12 +0400

    Remove all uncessary scripts from the back end

--- ServerSideApp/ServerSideApp/Controllers/HomeController.cs
+++ ServerSideApp/ServerSideApp/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
 using DevExpress.XtraReports.Web.WebDocumentViewer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace ServerSideApp.Controllers {
     public class HomeController : Controller {
+
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public IActionResult Index() {
+            var applicationUrls = System.Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+            ViewBag.ApplicationUrls = applicationUrls;
             return View();
         }
         public IActionResult Error() {
@@ -11,13 +21,5 @@ namespace ServerSideApp.Controllers {
             return View(model);
         }
         
-
-        public IActionResult DocumentViewer(
-            [FromServices] IWebDocumentViewerClientSideModelGenerator viewerModelGenerator,
-            [FromQuery] string reportName) {
-            reportName = string.IsNullOrEmpty(reportName) ? "TestReport" : reportName;
-            var viewerModel = viewerModelGenerator.GetModel(reportName, CustomWebDocumentViewerController.DefaultUri);
-            return View(viewerModel);
-        }
     }
 }
